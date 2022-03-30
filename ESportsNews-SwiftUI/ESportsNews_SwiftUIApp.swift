9fd5847df4fccde07a7d04173e7d227a9ca46b0f
Kaan İzgi <kaanizgi1531@gmail.com>
2022-03-30 14:02:06 +0300

    Initial Commit

---
+++ ESportsNews-SwiftUI/ESportsNews_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESportsNews_SwiftUIApp.swift
+//  ESportsNews-SwiftUI
+//
+//  Created by Kaan İzgi on 30.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ESportsNews_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
